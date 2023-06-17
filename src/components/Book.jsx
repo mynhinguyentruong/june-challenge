c6fc310221fa7e99eee9dbe00bc3d39f5a1c66fc
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-17 15:19:39 -0400

    Render one book detail

---
+++ src/components/Book.jsx
@@ -0,0 +1,27 @@
+import { useLoaderData } from "react-router-dom"
+
+export default function Book() {
+    const book = useLoaderData()
+
+    return (
+        <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 className="sr-only">Products</h2>
+  
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              <a key={book.id} href={`/book/${book.id}`} className="group">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                  <img
+                    src={book.imageSrc}
+                    alt={book.imageAlt}
+                    className="h-full w-full object-cover object-center group-hover:opacity-75"
+                  />
+                </div>
+                <h3 className="mt-4 text-sm text-gray-700">{book.name}</h3>
+                <p className="mt-1 text-lg font-medium text-gray-900">{book.price}</p>
+              </a>
+          </div>
+        </div>
+      </div>
+    )
+}

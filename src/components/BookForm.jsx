bcfa620b9ba522021698904a935e269bebd02c25
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-17 16:06:09 -0400

    Add handleChange logic

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -1,19 +1,21 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
+import { useState } from "react"
 
 export default function BookForm() {
+    const [form, setForm] = useState({
+        name: "",
+        price: "",
+        imageSrc: "",
+        imageAlt: ""
+    })
+
+    const handleChange = (e) => {
+        const name = e.target.name
+        setForm(prevForm => {
+            return {...prevForm, [name]: e.target.value}
+        })
+
+        console.log(form);
+    }
   return (
     <form>
       <div className="space-y-1 px-10 pt-10">
@@ -25,52 +27,58 @@ export default function BookForm() {
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-4">
-              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
                 What is your book title/name?
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset sm:max-w-md">
                   <input
                     type="text"
-                    name="username"
-                    id="username"
-                    autoComplete="username"
+                    value={form.name}
+                    name="name"
+                    id="name"
+                    autoComplete="name"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     placeholder="Book 3"
+                    onChange={handleChange}
                   />
                 </div>
               </div>
             </div>
 
             <div className="sm:col-span-4">
-              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-900">
                 How much you are selling it for?
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset sm:max-w-md">
                   <input
                     type="text"
-                    name="username"
-                    id="username"
-                    autoComplete="username"
+                    value={form.price}
+                    id="price"
+                    name="price"
+                    autoComplete="price"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     placeholder="$99"
+                    onChange={handleChange}
                   />
                 </div>
               </div>
             </div>
 
             <div className="sm:col-span-4">
-              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="imageSrc" className="block text-sm font-medium leading-6 text-gray-900">
                 What is your book image?
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset sm:max-w-md">
                   <input
                     type="text"
-                    name="username"
-                    id="username"
-                    autoComplete="username"
+                    value={form.imageSrc}
+                    id="imageSrc"
+                    name="imageSrc"
+                    onChange={handleChange}
+                    autoComplete="imageSrc"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     placeholder="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg"
                   />
@@ -79,16 +87,18 @@ export default function BookForm() {
             </div>
 
             <div className="sm:col-span-4">
-              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="imageAtl" className="block text-sm font-medium leading-6 text-gray-900">
                 Please provide an alt for your image!
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset sm:max-w-md">
                   <input
                     type="text"
-                    name="username"
-                    id="username"
-                    autoComplete="username"
+                    value={form.imageAlt}
+                    id="imageAlt"
+                    name="imageAlt"
+                    autoComplete="imageAtl"
+                    onChange={handleChange}
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     placeholder="A book with a fantastic cover"
                   />

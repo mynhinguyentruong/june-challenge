663f85c2d0e13f355768f694a7778e1c11d3c6a4
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-18 09:29:20 -0400

    Dispatch action with store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import booksReducer from "./features/books/booksSlice"
+import booksReducer, { fetchBooks } from "./features/books/booksSlice"
 
 export const store = configureStore({
   reducer: {
     books: booksReducer
   },
 })
+
+store.dispatch(fetchBooks())

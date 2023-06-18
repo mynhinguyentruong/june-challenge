4f929d363761e00d428b333e3f9a15f2a4ef16c5
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-06-18 09:28:41 -0400

    Clean up and switch to supabase anon key: as policy is added

--- src/features/books/booksSlice.js
+++ src/features/books/booksSlice.js
@@ -2,45 +2,15 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(import.meta.env.VITE_PROJECT_URL, import.meta.env.VITE_API_KEY)
+export const supabase = createClient(import.meta.env.VITE_PROJECT_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
+
 export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
     const { data: books, error } = await supabase.from('Books').select('*')
-
-    console.log({error});
-    console.log({books})
 
     if (error) return error
 
     return books
 })
-
-// const initialState = {
-//     value: [
-//         {
-//           id: 1,
-//           name: 'Book 1',
-//           price: '$48',
-//           imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-//           imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
-//         },
-//         {
-//           id: 2,
-//           name: 'Book 2',
-//           price: '$35',
-//           imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-//           imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
-//         },
-//         {
-//           id: 3,
-//           name: 'Book 3',
-//           price: '$89',
-//           imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-//           imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
-//         },
-    
-//         // More products...
-//       ]
-// }
 
 const initialState = {
     status: 'uninitialized',
